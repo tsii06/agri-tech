f05b8c2a4a06275c67dc05c3710764b42ecb1c02
Valyfaliana <valyandrianimpanana@gmail.com>
2025-03-25 20:31:40 +0300

    [ADD] Recolte dans ProducteurEnPhaseCulture - Decompose le contrat ProducteurEnPhaseCulture parce qu'elle est devenue trop lourde - Creation de la contrat ParcelleContrat et RecolteParcelle pour alleger ProducteurEnPhaseCulture

---
+++ agro-hardhat/contracts/RecolteContrat.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+
+contract RecolteContrat {
+
+    // ================================ variable d'etat ==============================================
+    enum Role { Producteur, Fournisseur, Certificateur, Collecteur, Auditeur, Transporteur, Exportateur }
+    enum Etape { PreCulture, Culture, Recolte, Transport }
+    enum ModePaiement { VirementBancaire, Cash, MobileMoney }
+
+    struct Acteur {
+        address addr;
+        Role role;
+    }
+
+    struct Intrant {
+        string nom;
+        uint32 quantite;
+        bool valide;
+    }
+
+    struct Inspection {
+        uint32 id;
+        address auditeur;
+        string rapport;
+        uint timestamp;
+    }
+
+    struct EnregistrementCondition {
+        uint32 id;
+        string temperature;
+        string humidite;
+        uint timestamp;
+    }
+
+    struct Parcelle {
+        uint32 id;
+        address producteur;
+        string qualiteSemence;
+        string methodeCulture;
+        bool certifie;
+        Etape etape;
+        string latitude;
+        string longitude;
+        string[] photos;
+        Intrant[] intrants;
+        Inspection[] inspections;
+        EnregistrementCondition[] conditions;
+        string dateRecolte;
+        string certificatPhytosanitaire;
+    }
+
+    struct Paiement {
+        uint32 id;
+        address payeur;
+        uint32 montant;
+        ModePaiement mode;
+        uint timestamp;
+    }
+
+    // ajout de struct recolte
+    struct Recolte {
+        uint32 id;
+        uint32 idParcelle;
+        uint32 quantite;
+        uint32 prix;
+        bool certifie;
+        string certificatPhytosanitaire;
+    }
+
+
+    // ------------------------- Attributs --------------------------------------------------------------
+    mapping(address => Acteur) public acteurs;
+    mapping(uint32 => Parcelle) public parcelles;
+    mapping(uint32 => Paiement) public paiements;
+    uint32 public compteurParcelles;
+    uint32 public compteurInspections;
+    uint32 public compteurConditions;
+    uint32 public compteurPaiements;
+    mapping(uint32 => Recolte) public recoltes;
+    uint32 public compteurRecoltes;
+    // ===============================================================================================
+
+
+    function ajoutRecolte(uint32 _idParcelle, uint32 _quantite, uint32 _prix) external {
+
+        require(_idParcelle <= compteurParcelles, "Parcelle non existant");
+
+        compteurRecoltes++;
+        recoltes[compteurRecoltes] = Recolte(compteurRecoltes, _idParcelle, _quantite, _prix, false, "");
+    }
+    function certifieRecolte(uint32 _idRecolte, string memory _certificat) external {
+
+        require(_idRecolte <= compteurRecoltes, "Recolte non existant");
+        require(bytes(_certificat).length != 0, "Certificat vide");
+
+        recoltes[_idRecolte].certifie = true;
+        recoltes[_idRecolte].certificatPhytosanitaire = _certificat;
+    }
+
+}
