c402ba341d623cb98a21158b2ed360b296e54055
Cornelius <valyandrianimpanana@gmail.com>
2025-06-14 14:30:53 +0300

    [UPDATE] met a jour les adresses dans contract.js du frontend

--- frontend-dapp/src/utils/contract.js
+++ frontend-dapp/src/utils/contract.js
@@ -6,11 +6,11 @@ import GestionnaireActeurs from "../abi/GestionnaireActeurs.json";
 
 // Adresses des contrats déployés sur le réseau local
 // Ces adresses sont obtenues après le déploiement avec le script deploy.js
-const PRODUCTEUR_PROXY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";  // ProducteurProxy
-const CollecteurExportateur_PROXY_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";  // CollecteurProxy
-const CollecteurProducteur_PROXY_ADDRESS = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"; 
+const PRODUCTEUR_PROXY_ADDRESS = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";  // ProducteurProxy
+const CollecteurExportateur_PROXY_ADDRESS = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853";  // CollecteurProxy
+const CollecteurProducteur_PROXY_ADDRESS = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788"; 
 // Adresse du contrat GestionnaireActeurs déployé sur le réseau local
-const GESTIONNAIRE_ACTEURS_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // À remplacer par la vraie adresse après déploiement
+const GESTIONNAIRE_ACTEURS_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // À remplacer par la vraie adresse après déploiement
 
 export async function getProvider() {
   if (!window.ethereum) {
