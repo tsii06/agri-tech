f68b4fb28409a554667a1eff67bd26640458e7da
Cornelius <valyandrianimpanana@gmail.com>
2025-06-22 20:06:00 +0300

    delete: dossier inutile

--- scripts/deploy.js
+++
@@ -1,8 +0,0 @@
-// ... existing code ...
-// Après avoir enregistré le producteur, déléguer le contrat ProducteurEnPhaseCulture
-await gestionnaireActeurs.connect(admin).ajouterContratDelegue(
-  adresseDuProducteur, // Remplace par la variable contenant l'adresse du producteur
-  adresseDuContratProducteurEnPhaseCulture // Remplace par la variable contenant l'adresse du contrat ProducteurEnPhaseCulture
-);
-// ... existing code ...
-// Ensuite, tu peux appeler creerParcelle sans erreur 
