e9fe02243279c3f2cbeb635d5fd9b796e7526e46
Cornelius <valyandrianimpanana@gmail.com>
2025-07-10 18:29:11 +0300

    [ADD] ajout upload de fichier certificat phytosanitaire dans creation parcelle

--- frontend-dapp/src/pages/ProducteurEnPhaseCulture/CreerParcelle.jsx
+++ frontend-dapp/src/pages/ProducteurEnPhaseCulture/CreerParcelle.jsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { getContract } from "../../utils/contract";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import myPinataSDK from "../../utils/pinata";
+import { useUserContext } from "../../context/useContextt";
+
+
 
 const defaultCenter = {
   lat: -18.8792,
@@ -27,7 +31,14 @@ function CreerParcelle() {
   const qualiteSemence = useRef("");
   const methodeCulture = useRef("");
   const dateRecolte = useRef("");
-  const certificatPhytosanitaire = useRef("");
+  // pour le certificat
+  const [certificat, setCertificat] = useState(null);
+  const dateEmission = useRef(null);
+  const dateExpiration = useRef(null);
+  const region = useRef(null);
+  const autorite_certificatrice = useRef(null);
+  // adresse de l'user
+  const { account } = useUserContext();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -36,14 +47,38 @@ function CreerParcelle() {
 
     try {
       const contract = await getContract();
+      let hashCertificat = ""; // hash du certificat sur ipfs
+      const idNewParcelle = await contract.getCompteurParcelle();
 
+      // UPLOADE CERTIFICAT PHYTOSANITAIRE
+      if (!certificat) {
+        throw new Error("Certificat phytosanitaire manquant");
+      } else {
+        try {
+          const upload = await myPinataSDK.upload.public.file(certificat).keyvalues({
+            dateEmission: dateEmission.current.value,
+            dateExpiration: dateExpiration.current.value,
+            region: region.current.value,
+            autorite_certificatrice: autorite_certificatrice.current.value,
+            adresseProducteur: account,
+            idParcelle: Number(idNewParcelle)
+          });
+          hashCertificat = upload.cid;
+        } catch (e) {
+          console.error("Erreur lors de l'upload du certificat parcelle : ", e);
+          setError("Impossible de créer la parcelle. Veuillez réessayer plus tard.");
+          return; // annule tous si il y a erreur lors de l'upload
+        }
+      }
+
+      // CREATION PARCELLE
       const tx = await contract.creerParcelle(
         qualiteSemence.current.value,
         methodeCulture.current.value,
         location.lat.toString(),
         location.lng.toString(),
         dateRecolte.current.value,
-        certificatPhytosanitaire.current.value
+        hashCertificat
       );
 
       await tx.wait();
@@ -84,11 +119,6 @@ function CreerParcelle() {
           <input type="date" className="form-control" required ref={dateRecolte} />
         </div>
 
-        <div className="mb-3">
-          <label className="form-label text-muted">Certificat phytosanitaire</label>
-          <input type="text" className="form-control" required ref={certificatPhytosanitaire} />
-        </div>
-
         <MapContainer center={location} zoom={12} style={{ height: '400px', width: '100%' }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -101,6 +131,43 @@ function CreerParcelle() {
         <div className="mt-3 text-muted">
           Position sélectionnée : {location.lat.toFixed(6)}, {location.lng.toFixed(6)}
         </div>
+
+        {/* CERTIFICAT PHYTOSANITAIRE */}
+        <fieldset className="mt-5">
+          <legend>Certificat phytosanitaire</legend>
+
+          <div className="mb-3">
+            <input type="file" className="form-control" required onChange={e => setCertificat(e.target.files[0])} />
+          </div>
+
+          <div className="row mb-3">
+            <div className="col">
+              <label className="form-label text-muted">Date d'emession</label>
+              <input type="date" className="form-control" required ref={dateEmission} />
+            </div>
+            <div className="col">
+              <label className="form-label text-muted">Date d'expiration</label>
+              <input type="date" className="form-control" required ref={dateExpiration} />
+            </div>
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="region" className="form-label text-muted">Region</label>
+            <select className="form-select" id="region" ref={region}>
+              <option>Antananarivo</option>
+              <option>Antsiranana</option>
+              <option>Antsirabe</option>
+              <option>Toamasina</option>
+              <option>Fianarantsoa</option>
+              <option>Toliara</option>
+            </select>
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="autorite_certificatrice" className="form-label text-muted">Autorité certificatrice</label>
+            <input type="text" className="form-control" required id="autorite_certificatrice" ref={autorite_certificatrice} />
+          </div>
+        </fieldset>
 
         <button
           disabled={loading}
