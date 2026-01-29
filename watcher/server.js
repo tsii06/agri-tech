705bac5b0d0403d218dbc62ccc232646ec97e1e9
Cornelius <valyandrianimpanana@gmail.com>
2026-01-30 00:35:51 +0300

    [update/watcher] ajout de nouveau deps pour la db

--- watcher/server.js
+++ watcher/server.js
@@ -8,8 +8,18 @@ app.get("/expedition/:ref", async (req, res) => {
   try {
     const ref = req.params.ref;
     const data = await getExpeditionData(ref);
-    res.json(data);
+    res.json(
+      JSON.parse(
+        JSON.stringify(data, (_, value) =>
+          typeof value === "bigint" ? value.toString() : value
+        )
+      )
+    );
   } catch (err) {
+    console.error(
+      "Erreur lors de la recuperation des donnees depuis le smart contract privee : ",
+      err.message
+    );
     res.status(500).json({ error: err.message });
   }
 });
